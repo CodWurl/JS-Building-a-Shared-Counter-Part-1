8b0bbd567d872141f05d4124503e5bd60b00a470
CodWurl <46231725+CodWurl@users.noreply.github.com>
2022-06-12 18:23:16 -0400

    added more solution in index.js

--- public/index.js
+++ public/index.js
@@ -1,18 +1,42 @@
-function main(){
+async function main(){
     const countContainer = document.querySelector('#count-container');
     const incrementButton = document.querySelector('#increment-button');
     const decrementButton = document.querySelector('#decrement-button');
 
-    let countValue = 0;
+    const response = await fetch('http://localhost:9001/counter');
+
+    const result = await response.json();
+    
+    let countValue = result.value;
 
     function increment(){
         countValue++;
         countContainer.textContent = countValue;
+
+        fetch('http://localhost:9001/counter', {
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                value: countValue
+            })
+        })
     }
 
     function decrement(){
         countValue--;
         countContainer.textContent = countValue;
+
+        fetch('http://localhost:9001/counter', {
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                value: countValue
+            })
+        })
     }
 
     incrementButton.addEventListener('click', increment);
